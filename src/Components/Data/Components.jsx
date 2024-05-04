a9378be8cde7593700f806e20cf75b915af992a7
ahmedelzahaby <ahmed.elzahaby@bld.ai>
2024-05-04 13:03:17 +0300

    edit z-indexing

--- src/Components/Data/Components.jsx
+++ src/Components/Data/Components.jsx
@@ -4,7 +4,6 @@ export const DataContainer = styled.div`
   flex: 1;
   padding: 10px 0px 0px 20px;
   position: relative;
-  z-index: -1;
   width: 100%;
   @media (max-width: 504px) {
     padding: 5px;
